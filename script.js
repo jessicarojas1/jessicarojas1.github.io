2218088a87acbd1004eefbfd23e9e00ad0c738fa
Jessica Rojas <35229083+jessicarojas1@users.noreply.github.com>
2025-06-19 18:43:25 +0000

    Animation to the Timeline page

--- script.js
+++ script.js
@@ -2,18 +2,3 @@ document.getElementById('darkModeToggle').addEventListener('click', function () 
   document.body.classList.toggle('dark-mode');
   this.textContent = document.body.classList.contains('dark-mode') ? '☀️ Light Mode' : '🌙 Dark Mode';
 });
-
-
-// Scroll animation
-document.addEventListener("DOMContentLoaded", () => {
-  const cards = document.querySelectorAll(".project-card");
-  const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add("visible");
-      }
-    });
-  }, { threshold: 0.1 });
-
-  cards.forEach(card => observer.observe(card));
-});
