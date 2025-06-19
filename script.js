65eda0319ca21ccd60f1902d14613f00ce0b731c
Jessica Rojas <35229083+jessicarojas1@users.noreply.github.com>
2025-06-19 18:55:14 +0000

    COMMIT

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ document.getElementById('darkModeToggle').addEventListener('click', function () 
   document.body.classList.toggle('dark-mode');
   this.textContent = document.body.classList.contains('dark-mode') ? '☀️ Light Mode' : '🌙 Dark Mode';
 });
+
+function toggleDescription(block) {
+  const content = block.querySelector('.timeline-content');
+  content.classList.toggle('expanded');
+}
